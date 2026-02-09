a21fb73cc5cfe59de4762622aa827c4b3ea9ee3b
Melton122 <meltonhlungwani970@gmail.com>
2026-02-09 15:23:46 +0200

    Configure supabase environment variables and unignore config file

--- supabaseConfig.js
+++ supabaseConfig.js
@@ -1,11 +1,10 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import Constants from 'expo-constants';
 
-// Use environment variables from EAS build
-const supabaseUrl = Constants.expoConfig.extra.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = Constants.expoConfig.extra.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+// No need for Constants.expoConfig.extra anymore!
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
